3ee39aa4b566b12b68a022292d3ff773e6672198
Dave Goehrig <dgoehrig@critical-media.com>
2013-06-13 10:11:33 -0400

    Adding bucket_cache_hook.erl
    Updating README with usage instructions
    Providing basic bucket_cache_hook.erl

--- README.md
+++ README.md
@@ -2,3 +2,35 @@ bucket_cache_hook
 =================
 
 This is a sample post commit hook for Riak that generates an aggregate JSON representation of the contents of a bucket.
+
+Installation
+============
+
+The easiest way to install the bucket_cache_hook is to copy it into the root directory of your riak installation and:
+
+	# start riak and compile the bucket_cache_hook.erl
+	$ ./bin/riak console
+	(riak1@127.0.0.1)1> c(bucket_cache_hook).
+	(riak1@127.0.0.1)2> q().
+
+	# then add it as a postcommit hook to your chosen bucket
+	$ curl -XPUT -H "Content-Type: application/json" http://localhost:8091/riak/test -d '{"props":{"postcommit":[{"mod": "bucket_cache_hook", "fun": "create_cache"}]}}'
+
+	# now load a test payload
+	$ curl -XPUT -H "Content-Type: applicaiton/json" http://localhost:8091/riak/test/test.json -d '{ "message": "this is a test" }'
+	
+	# then check that it loaded
+	$ curl http://localhost:8091/riak/test/test.json
+	{ "message": "this is a test" }
+
+	# next  check the cache, you'll find [ {filename : payload } ]
+	$ curl http://localhost:8091/riak/cache/test
+	[{"test.json":{"message":"this is a test"}}]
+
+	# then clean up after yourself
+	$ curl -XDELETE http://localhost:8091/riak/test/test.json
+	$ curl http://localhost:8091/riak/cache/test
+	[]
+
+David J. Goehrig
+
---
+++ bucket_cache_hook.erl
@@ -0,0 +1,43 @@
+-module(bucket_cache_hook).
+-author({ "David J Goehrig", "dave@dloh.org" }).
+-copyright("© 2012 David J. Goehrig").
+-export([ create_cache/1 ]).
+
+% the bucket_cache_hook is a Riak post commit hook that will take json objects from a bucket and install into
+% a /riak/cache/[bucket] file as an array of elements.  This is useful for when you want to have a prebaked
+% JSON representation of all of the objects in a bucket.
+
+create_cache(Object) ->
+	{ok, C} = riak:local_client(),							%% instantiate a local client
+	Bucket = erlang:iolist_to_binary([riak_object:bucket(Object)]),			%% grab the name of the bucket of the new object
+	Key = erlang:iolist_to_binary([riak_object:key(Object)]),
+	CacheJSON = case C:get(<<"cache">>,Bucket) of					%% we then grab out of the cache object the bucket's cace 
+		{ ok, OldCache } -> riak_object:get_value(OldCache);
+		{ error, notfound } -> <<"[]">>;
+		{ error, Reason } ->  throw({ error, Reason })				%% can't fix it, something is really wrong!
+	end,
+	Cache = mochijson2:decode(CacheJSON),						%% and extract the JSON representation of the cache
+	NewCacheJSON = case is_deleted(Object) of 
+		true -> iolist_to_binary(mochijson2:encode(delete(Cache,Key)));
+		false -> update(Cache,Key,Object)
+	end,
+	NewCache = riak_object:new(<<"cache">>,Bucket,NewCacheJSON,"application/json"),
+	C:put(NewCache).								%% install the new cache
+
+is_deleted(Object) ->
+	MetaData = riak_object:get_metadata(Object),
+	dict:is_key(<<"X-Riak-Deleted">>,MetaData).
+
+delete(Cache,Key) ->
+	delete(Cache,Key,[]).
+delete([{struct,[{ Key, _ }]} | Tail ], Key,Accumulator) ->
+	delete(Tail,Key,Accumulator);
+delete([ Head | Tail ], Key, Accumulator) ->
+	delete(Tail,Key,[ Head | Accumulator]);
+delete([],_Key,Accumulator) ->
+	Accumulator.
+
+update(Cache,Key,Object) ->
+	ObjectJSON = riak_object:get_value(Object),				%% get the contents of the newly added object
+	O = mochijson2:decode(ObjectJSON),					%% and parse the json of the new object
+	iolist_to_binary(mochijson2:encode([ { struct,[ { Key, O }]} | delete(Cache,Key) ])).
